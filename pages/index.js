ff9a19dac9ec0fac7afc813cf6ff1a30ceb560a2
jyotiprakash-m <teachmeonline365@gmail.com>
2021-07-02 08:15:02 +0530

    search functionality works

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,53 @@ import { useState } from "react";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import styles from "../styles/Home.module.css";
 import DisplayCharecter from "../components/DisplayCharecter";
-
-
+import { message, Divider, Input, Space } from 'antd';
+const { Search } = Input;
+import { CloseSquareOutlined } from '@ant-design/icons';
 
 export default function Home(results) {
   const intialState = results;
   const [characters, setCharacters] = useState(intialState.characters);
+  const [search, setSearch] = useState("")
+  const onSearch = async (value) => {
+    const client = new ApolloClient({
+      uri: "https://rickandmortyapi.com/graphql/",
+      cache: new InMemoryCache(),
+    });
+    const { data } = await client.query({
+      query: gql`
+        query {
+          characters(filter: { name: "${value}" }) {
+            info {
+              count
+            }
+            results {
+              name
+              id
+              location {
+                name
+                id
+              }
+              image
+              origin {
+                name
+                id
+              }
+              episode {
+                id
+                episode
+                air_date
+              }
+            }
+          }
+        }
+      `
+    })
+
+    setCharacters(data.characters.results);
+
+  };
+
   return (
     <div>
       <Head>
@@ -17,6 +58,18 @@ export default function Home(results) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{ padding: "5%" }}>
+        <Space direction="horizontal" align="center">
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+
+          />
+
+        </Space>
+        <Divider />
 
         <DisplayCharecter characters={characters} />
       </div>
